3b71ed8bc78a1fab6ab81bec4cc82cf427b3935e
Wassim BOUGARFA <wbougarfa@vermeg.com>
2020-06-05 13:23:39 +0200

    added path alias

--- apps/hrms-api/src/app.module.ts
+++ apps/hrms-api/src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
+import { CoreModule } from '@hrms-core/core.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DBModule } from 'apps/hrms-db/src/db.module';
-import { CoreModule } from 'apps/hrms-core/src/core.module';
 
 @Module({
   imports: [
--- apps/hrms-core/src/core.module.ts
+++ apps/hrms-core/src/core.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
+import { DBModule } from '@hrms-db/db.module';
 import { UserManagementModule } from './modules/user-management/user-management.module';
 import { ProfileManagementModule } from './modules/profile-management/profile-management.module';
 import { ConfigurationModule } from './modules/configuration/configuration.module';
-import { DBModule } from 'apps/hrms-db/src/db.module';
 
 
 @Module({
