bc542430e75e5dc8d1a61739214ec004214b2f0a
Wassim Bougarfa <wbougarfa@nuevera.com>
2020-07-19 19:34:57 +0200

    role controller return https status

--- apps/hrms-api/src/config-management/controllers/role.controller.ts
+++ apps/hrms-api/src/config-management/controllers/role.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Res, HttpStatus, Query, Param, Delete } fr
 import { RoleDto } from '@hrms-core/dto/role.dto';
 import { RoleFacade, RoleFilterCriteria } from '@hrms-core/modules/config-management/facades/role.facade';
 import { Response } from 'express';
+import { ErrorUtils } from '@hrms-api/common/error.utils';
 
 @Controller('/roles')
 export class RoleController {
@@ -33,27 +34,27 @@ export class RoleController {
     async createRole(@Body() roleDto: RoleDto, @Res() response: Response) {
         await this.roleFacade.createRole(roleDto)
             .then(role => response.json(role))
-            .catch(err => response.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err));
+            .catch(err => response.status(ErrorUtils.responseCode(err)).json(err));
     }
 
     @Post('/update')
     async updateRole(@Body() roleDto: RoleDto, @Res() response: Response) {
         await this.roleFacade.updateRole(roleDto)
             .then(role => response.json(role))
-            .catch(err => response.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err));
+            .catch(err => response.status(ErrorUtils.responseCode(err)).json(err));
     }
 
     @Get('/role/:roleId')
     async getDetails(@Param('roleId') roleId: string, @Res() response: Response) {
         await this.roleFacade.roleDetails(roleId)
             .then(role => response.status(HttpStatus.OK).json(role))
-            .catch(err => response.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err))
+            .catch(err => response.status(ErrorUtils.responseCode(err)).json(err))
     }
 
     @Delete('/role/:roleId')
     async deleteRole(@Param('roleId') roleId: string, @Res() response: Response) {
         await this.roleFacade.deleteRole(roleId)
             .then(role => response.status(HttpStatus.OK).json(role))
-            .catch(err => response.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err))
+            .catch(err => response.status(ErrorUtils.responseCode(err)).json(err))
     }
 }
