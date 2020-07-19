95b4bf87982c40de3e730648400dbd3f47f94fc5
Wassim Bougarfa <wbougarfa@nuevera.com>
2020-07-19 19:34:42 +0200

    Infer http status code base on error code

---
+++ apps/hrms-api/src/common/error.utils.ts
@@ -0,0 +1,19 @@
+import { ErrorDto } from "@hrms-core/common/services/dto/error-dto.service";
+import { HttpStatus } from "@nestjs/common";
+
+export class ErrorUtils {
+    static responseCode(err: ErrorDto) {
+
+        if (!err?.statusCode) {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        const errorCode = err.statusCode.toString();
+
+        if (errorCode[0] == '5') {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        } else if (errorCode[0] == '4') {
+            return HttpStatus.BAD_REQUEST;
+        };
+    }
+}
