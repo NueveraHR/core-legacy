f1a43c6fca11827a431639bd395b439d4dd18696
Wassim Bougarfa <wbougarfa@nuevera.com>
2020-08-23 00:42:21 +0200

    export seperated job module

--- apps/hrms-core/src/core/core.module.ts
+++ apps/hrms-core/src/core/core.module.ts
@@ -1,8 +1,5 @@
-import { JobService } from './job/job.service';
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { HRMSConfigModule } from '@libs/config';
-import { Job, JobSchema } from './job/job.schema';
 import { UserModule } from './user/user.module';
 import { RoleModule } from './role/role.module';
 import { EmployeeModule } from './employee/employee.module';
@@ -10,16 +7,10 @@ import { EmployeeModule } from './employee/employee.module';
 @Module({
   imports: [
     HRMSConfigModule,
-    MongooseModule.forFeature([
-      { name: Job.name, schema: JobSchema },
-    ]),
 
     RoleModule,
     UserModule,
     EmployeeModule
-  ],
-  providers: [
-    JobService,
   ],
   exports: [
     RoleModule,
---
+++ apps/hrms-core/src/core/job/job.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { JobService } from './job.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Job, JobSchema } from './job.schema';
+
+@Module({
+    imports: [
+        MongooseModule.forFeature([{ name: Job.name, schema: JobSchema }]),
+    ],
+    providers: [
+        JobService,
+    ],
+    exports: [
+        JobService,
+    ]
+})
+export class JobModule { };
