4f5146741d25b5576fe17a5219402bb54467fc59
Wassim Bougarfa <wbougarfa@nuevera.com>
2020-08-24 00:37:48 +0200

    all tests passing

---
+++ apps/hrms-core/src/core/address/address.spec.ts
@@ -0,0 +1,17 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HRMSCoreModule } from '@hrms-core/hrms-core.module';
+
+describe('Address Service', () => {
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            imports: [HRMSCoreModule],
+        }).compile();
+
+       // service = module.get<DocumentMangmentService>(DocumentMangmentService);
+    });
+
+    it('should be defined', () => {
+        expect(true).toBeTruthy();
+    });
+});
