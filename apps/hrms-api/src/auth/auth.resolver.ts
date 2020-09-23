5352c59565d74cb0eb24771d06879ee1319ca06c
Wassim Bougarfa <wbougarfa@nuevera.com>
2020-09-24 00:57:18 +0100

    fix cors settings

--- apps/hrms-api/src/auth/auth.resolver.ts
+++ apps/hrms-api/src/auth/auth.resolver.ts
@@ -4,6 +4,7 @@ import { UserCredentials } from './auth.input';
 import { AuthPayload } from './auth.type';
 import { AuthenticationError } from 'apollo-server';
 import { EnvData, EnvService } from '@libs/env';
+import * as ms from 'ms';
 
 @Resolver()
 export class AuthResolver {
@@ -16,13 +17,18 @@ export class AuthResolver {
             .auth(credentials)
             .then(authDto => {
                 const env = this.envService.read();
+                const maxAge = this.getMaxAge(env.JWT_EXPIRESIN);
                 const authCookie = `Authentication=${authDto.token}; HttpOnly; ${
                     this.envService.isProd() ? 'secure;' : ''
-                } Path=/; Max-Age=${env.JWT_EXPIRESIN}`;
+                } Path=/; Max-Age=${maxAge}`;
 
                 res.setHeader('Set-Cookie', authCookie);
                 return authDto;
             })
             .catch(err => new AuthenticationError(err.message));
+    }
+
+    private getMaxAge(expiresIn: string): any {
+        return expiresIn ? ms(expiresIn) : '';
     }
 }
