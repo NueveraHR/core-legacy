0d4be557de24d947b8d36b049f844882e3d3d17d
Wassim Bougarfa <wbougarfa@nuevera.com>
2020-09-25 12:29:03 +0100

    Rename guard

--- apps/hrms-api/src/employee/employee.resolver.ts
+++ apps/hrms-api/src/employee/employee.resolver.ts
@@ -1,7 +1,7 @@
 import { Resolver, Query, Mutation, Args, ID, Int } from '@nestjs/graphql';
 import { EmployeeFacade } from '@hrms-core/facades/employee.facade';
 import { UseGuards } from '@nestjs/common';
-import { GqlAuthGuard } from '@hrms-api/common/guards/gql-auth.guard';
+import { JwtAuthGuard } from '@hrms-api/common/guards/auth.guard';
 import { Privileges } from '@hrms-api/common/decorators/privileges.decorator';
 import { PrivilegesGuard } from '@hrms-api/common/guards/role.guard';
 import { AddEmployeeInput, UpdateEmployeeInput, JobInput } from '@hrms-api/employee/employee.input';
@@ -13,7 +13,7 @@ import { FilterInput } from '@hrms-api/common/graphql/filter.input';
 
 @Resolver()
 @Privileges('employees.access')
-@UseGuards(GqlAuthGuard, PrivilegesGuard)
+@UseGuards(JwtAuthGuard, PrivilegesGuard)
 export class EmployeeResolver {
     constructor(private employeeFacade: EmployeeFacade) {}
 
--- apps/hrms-api/src/upload/upload.resolver.ts
+++ apps/hrms-api/src/upload/upload.resolver.ts
@@ -3,7 +3,7 @@ import { Args, Mutation, Resolver, Query, ID } from '@nestjs/graphql';
 import { GraphQLUpload } from 'apollo-server-core';
 import { FileUpload } from 'graphql-upload';
 
-import { GqlAuthGuard } from '@hrms-api/common/guards/gql-auth.guard';
+import { JwtAuthGuard } from '@hrms-api/common/guards/auth.guard';
 import { UseGuards } from '@nestjs/common';
 import { CurrentUser } from '../common/decorators/currentUser.decorator';
 import { DocumentMangmentService } from '@hrms-core/core/document/document-mangment.service';
@@ -12,87 +12,77 @@ import { UploadDocument, DeleteFileResult, GetDocument, UploadProfileImage } fro
 import { DocumentDto } from '@hrms-core/dto/document.dto';
 import { UserService } from '@hrms-core/core/user/user.service';
 
-@UseGuards(GqlAuthGuard)
+@UseGuards(JwtAuthGuard)
 @Resolver()
 export class UploadResolver {
-  constructor(
-    private readonly documentMangmentService: DocumentMangmentService,
-    protected userService: UserService
-  ) { }
+    constructor(
+        private readonly documentMangmentService: DocumentMangmentService,
+        protected userService: UserService,
+    ) {}
 
-
-  @Query(() => GetDocument)
-  async getDocument(
-    @Args('id', { type: () => String }) id: string,
-  ): Promise<GetDocument> {
-    return this.documentMangmentService.findById(id)
-  }
-
-  @Mutation(() => UploadDocument)
-  public async uploadDocument(
-    @CurrentUser() currentUser: UserDto,
-    @Args('name', { type: () => String }) name: string,
-    @Args('description', { type: () => String }) description: string,
-    @Args('file', { type: () => GraphQLUpload }) file: FileUpload
-  ): Promise<UploadDocument> {
-
-    const fileData = {
-      description,
-      name,
-      userId: currentUser.id
+    @Query(() => GetDocument)
+    async getDocument(@Args('id', { type: () => String }) id: string): Promise<GetDocument> {
+        return this.documentMangmentService.findById(id);
     }
-    const savedFile = await this.documentMangmentService.save(file, fileData);
-    return {
-      description: savedFile.description,
-      id: savedFile.id,
-      name: savedFile.name,
-      path: savedFile.path,
-      type: savedFile.type
-    };
-  }
 
-  @Mutation(() => UploadProfileImage)
-  public async uploadProfileImage(
-    @Args('employeeId', { type: () => ID }) employeeId: string,
-    @Args('file', { type: () => GraphQLUpload }) file: FileUpload
-  ): Promise<UploadProfileImage> {
-    const imgPath = await this.documentMangmentService.uploadToImgpush(file);
-    await this.userService.updatePicture(employeeId, imgPath);
-
-    return {
-      imagePath: imgPath
-    };
-  }
-
-  @Mutation(() => UploadDocument)
-  public async updateDocument(
-    @Args('id', { type: () => String }) id: string,
-    @Args('name', { type: () => String, nullable: true }) name: string,
-    @Args('description', { type: () => String, nullable: true }) description: string
-  ): Promise<UploadDocument> {
-
-    const fileData: DocumentDto = {
-      id,
-      name,
-      description
+    @Mutation(() => UploadDocument)
+    public async uploadDocument(
+        @CurrentUser() currentUser: UserDto,
+        @Args('name', { type: () => String }) name: string,
+        @Args('description', { type: () => String }) description: string,
+        @Args('file', { type: () => GraphQLUpload }) file: FileUpload,
+    ): Promise<UploadDocument> {
+        const fileData = {
+            description,
+            name,
+            userId: currentUser.id,
+        };
+        const savedFile = await this.documentMangmentService.save(file, fileData);
+        return {
+            description: savedFile.description,
+            id: savedFile.id,
+            name: savedFile.name,
+            path: savedFile.path,
+            type: savedFile.type,
+        };
     }
-    const savedFile = await this.documentMangmentService.update(fileData);
-    return {
-      description: savedFile.description,
-      id: savedFile.id,
-      name: savedFile.name,
-      path: savedFile.path,
-      type: savedFile.type
-    };
-  }
 
-  @Mutation(() => DeleteFileResult)
-  public async deleteDocument(
-    @Args('id', { type: () => String }) id: string
-  ): Promise<DeleteFileResult> {
-    return { success: await this.documentMangmentService.delete(id) }
-  }
+    @Mutation(() => UploadProfileImage)
+    public async uploadProfileImage(
+        @Args('employeeId', { type: () => ID }) employeeId: string,
+        @Args('file', { type: () => GraphQLUpload }) file: FileUpload,
+    ): Promise<UploadProfileImage> {
+        const imgPath = await this.documentMangmentService.uploadToImgpush(file);
+        await this.userService.updatePicture(employeeId, imgPath);
 
+        return {
+            imagePath: imgPath,
+        };
+    }
 
+    @Mutation(() => UploadDocument)
+    public async updateDocument(
+        @Args('id', { type: () => String }) id: string,
+        @Args('name', { type: () => String, nullable: true }) name: string,
+        @Args('description', { type: () => String, nullable: true }) description: string,
+    ): Promise<UploadDocument> {
+        const fileData: DocumentDto = {
+            id,
+            name,
+            description,
+        };
+        const savedFile = await this.documentMangmentService.update(fileData);
+        return {
+            description: savedFile.description,
+            id: savedFile.id,
+            name: savedFile.name,
+            path: savedFile.path,
+            type: savedFile.type,
+        };
+    }
 
+    @Mutation(() => DeleteFileResult)
+    public async deleteDocument(@Args('id', { type: () => String }) id: string): Promise<DeleteFileResult> {
+        return { success: await this.documentMangmentService.delete(id) };
+    }
 }
